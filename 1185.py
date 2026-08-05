62e2bd90ac6e59ecb144b736a692b8df01af5a14
Allice Albuquerque <allice.ianne@upe.br>
2026-08-05 20:13:03 -0300

    Add updates

---
+++ 1185.py
@@ -0,0 +1,18 @@
+O = input()
+matriz = []
+
+for i in range(12):
+    linha = []
+    for j in range(12):
+        linha.append(float(input()))
+    matriz.append(linha)
+
+soma = 0
+for i in range(12):
+    for j in range(0,11-i):
+        soma += matriz[i][j]
+
+if O == 'S':
+    print(f'{soma:.1f}')
+else:
+    print(f'{soma/66:.1f}')
